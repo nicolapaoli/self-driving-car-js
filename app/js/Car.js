572c4dd2ced36d086b0cee00f80afa958927c413
Nicola Paoli <nicolapaoli.work@gmail.com>
2024-03-27 23:24:21 +0000

    Make car as polygon
    Signed-off-by: Nicola Paoli <nicolapaoli.work@gmail.com>

--- app/js/Car.js
+++ app/js/Car.js
@@ -19,7 +19,47 @@ class Car {
 
     update(roadBorders) {
         this.#move()
+        this.polygon = this.#createPolygon()
         this.sensor.update(roadBorders)
+    }
+
+    #createPolygon() {
+        // method to find corner points of the car
+        const points = []
+
+        // find radius of the car, which is basically half of its hypot
+        const rad = Math.hypot(this.width, this.height) / 2
+        // find alpha angle between hypot and center
+        const alpha = Math.atan2(this.width, this.height)
+
+        // Top right point
+        points.push({
+            x: this.x - Math.sin(this.angle - alpha) * rad, //combining the 2 angles
+            y: this.y - Math.cos(this.angle - alpha) * rad
+        })
+        // Point 2
+        points.push({
+            x: this.x - Math.sin(this.angle + alpha) * rad, //combining the 2 angles
+            y: this.y - Math.cos(this.angle + alpha) * rad
+        })
+        // Point 3
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad, //combining the 2 angles
+            y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad
+        })
+
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle) * rad*.7, //combining the 2 angles
+            y: this.y - Math.cos(Math.PI + this.angle) * rad*.7
+        })
+
+        // Point 4
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad, //combining the 2 angles
+            y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad
+        })
+
+        return points
     }
 
     #move() {
@@ -55,20 +95,32 @@ class Car {
     }
 
     draw(context) {
-        context.save()
-        context.translate(this.x, this.y)
-        context.rotate(-this.angle)
+        // Draw a rectangle
+        // context.save()
+        // context.translate(this.x, this.y)
+        // context.rotate(-this.angle)
+        //
+        // context.beginPath()
+        // context.rect(
+        //     -this.width/2,
+        //     -this.height/2,
+        //     this.width,
+        //     this.height
+        // )
+        // context.fill();
 
         context.beginPath()
-        context.rect(
-            -this.width/2,
-            -this.height/2,
-            this.width,
-            this.height
-        )
-        context.fill();
+        context.fillStyle = 'red'
+        // context.strokeStyle = 'black'
 
-        context.restore()
+        context.moveTo(this.polygon[0].x, this.polygon[0].y)
+        for (let i=1;i < this.polygon.length; i++) {
+            context.lineTo(this.polygon[i].x, this.polygon[i].y)
+        }
+        context.lineTo(this.polygon[0].x, this.polygon[0].y)
+
+        context.stroke()
+        context.fill()
 
         this.sensor.draw(context)
     }
